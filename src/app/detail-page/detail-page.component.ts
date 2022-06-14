16eee0801bd42a53c6a8355b13d92cb31a657204
tunnelrat16 <tjandmatt@hotmail.com>
2022-06-13 19:32:37 -0700

    nothing is working

--- src/app/detail-page/detail-page.component.ts
+++ src/app/detail-page/detail-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Figure } from '../models/Figure';
 import { ActivatedRoute } from '@angular/router';
-
+import { Location } from '@angular/common';
 import { FigureService } from '../figure.service';
 
 @Component({
@@ -10,22 +10,26 @@ import { FigureService } from '../figure.service';
   styleUrls: ['./detail-page.component.scss'],
 })
 export class DetailPageComponent implements OnInit {
-  @Input() figure?: Figure;
+figure: Figure | undefined;
 
-  figures: Figure[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private figureService: FigureService,
+    private location: Location,
   ) {}
 
   ngOnInit(): void {
     this.getFigure();
+
   }
 
   getFigure(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.figureService.getFigure(id)
-      .subscribe(figure => this.figure = figure);
+      .subscribe(figure => {console.log(figure);
+        this.figure = figure;
+
+      })
   }
 }
--- src/app/figure.service.ts
+++ src/app/figure.service.ts
@@ -1,60 +1,65 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import {Figure} from './models/Figure';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Figure } from './models/Figure';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
 
 type FiguresResponse = {
   figures: Figure[];
-}
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FigureService {
-  figures: Figure[] = []
+  // figures: Figure[] = [];
 
-  /** GET figures from the server */
-getFigures(): Observable<Figure[]> {
-  return this.http.get<Figure[]>(this.figuresURL)
-    .pipe(
-      tap(_ => this.log('fetched figures')),
-      catchError(this.handleError<Figure[]>('getFigures', []))
-    );
-}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
-/** GET figure by id. Will 404 if id not found */
-getFigure(id: number): Observable<Figure> {
-  const url = `${this.figuresURL}/${id}`;
-  return this.http.get<Figure>(url).pipe(
-    tap(_ => this.log(`fetched figure id=${id}`)),
-    catchError(this.handleError<Figure>(`getFigure id=${id}`))
-  );
-}
+  private figuresUrl = 'https://gijoe-api.herokuapp.com/api/gijoe';
 
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-    console.error(error);
-    this.log(`${operation} failed: ${error.message}`);
-    return of(result as T);
+  fetchFigures() {
+    return this.http.get<FiguresResponse>(this.figuresUrl);
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
-}
-
-constructor(private http: HttpClient, private messageService: MessageService) { }
-
-private figuresURL = "https://gijoe-api.herokuapp.com/api/gijoe"
 
 
-fetchFigures() {
-  return this.http.get<FiguresResponse>(this.figuresURL)
-}
+  // /** GET figures from the server */
+  // getFigures(): Observable<Figure[]> {
+  //   return this.http.get<Figure[]>(this.figuresUrl).pipe(
+  //     tap((_) => this.log('fetched figures')),
+  //     catchError(this.handleError<Figure[]>('getFigures', []))
+  //   );
+  // }
+
+  /** GET figure by id. Will 404 if id not found */
+  getFigure(id: number): Observable<Figure> {
+    const url = `${this.figuresUrl}/${id}`;
+    return this.http.get<Figure>(url).pipe(
+      map((figure) => {
+        return figure;
+      })
+    );
+  }
+
+  // private handleError<T>(operation = 'operation', result?: T) {
+  //   return (error: any): Observable<T> => {
+  //     console.error(error);
+  //     this.log(`${operation} failed: ${error.message}`);
+  //     return of(result as T);
+  //   };
+  // }
 
 
-
-    /** Log a HeroService message with the MessageService */
-    private log(message: string) {
-      this.messageService.add(`FigureService: ${message}`);
-    }
-
+  /** Log a HeroService message with the MessageService */
+  // private log(message: string) {
+  //   this.messageService.add(`FigureService: ${message}`);
+  // }
 }
