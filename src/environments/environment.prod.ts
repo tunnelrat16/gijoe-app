3dd04cb4fa9e67f069a1b8747f4f1c13383b9ea8
tunnelrat16 <tjandmatt@hotmail.com>
2022-06-16 10:47:06 -0700

    Remove Environment Data

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
-  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-
-};
