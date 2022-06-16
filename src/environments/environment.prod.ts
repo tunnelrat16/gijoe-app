2478ce4eef1825bf1e68b70244388c868928f301
tunnelrat16 <tjandmatt@hotmail.com>
2022-06-16 09:42:12 -0700

    Verify Environment Variables Not Shared

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '96e281fb50msh8dd1a6d29293704p12cf84jsn274e655e6717'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,11 @@
 
 export const environment = {
   production: false,
-
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '96e281fb50msh8dd1a6d29293704p12cf84jsn274e655e6717'
 };
 
 
