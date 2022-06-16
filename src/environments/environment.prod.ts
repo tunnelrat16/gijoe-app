ca51d6a09e3c639aa55c97e951a9a8125c19e67e
tunnelrat16 <tjandmatt@hotmail.com>
2022-06-16 09:47:16 -0700

    Clear Environment Files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
   XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: '96e281fb50msh8dd1a6d29293704p12cf84jsn274e655e6717'
+
 };
